09364526c240d07d370c44bbfd15a3304aa723e3
arshadvkb <arshadvkb118@gmail.com>
2025-11-02 20:16:49 +0530

    logout finished

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -94,4 +94,18 @@ const login = async (req, res) => {
   }
 };
 
-export { register, login };
+
+const logout = async (req, res) => {
+  try {
+    res.clearCookie('jwt', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    });
+    return res.json({ success: true, message: 'Logged out' });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+export { register, login, logout };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { login, register } from '../controllers/auth.controller.js';
+import { login, logout, register } from '../controllers/auth.controller.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/user/register', register);
 authRouter.post('/login', login);
+authRouter.post('/logout', logout);
+
 
 export default authRouter;
