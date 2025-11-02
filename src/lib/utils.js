44aa8b64e3f345e57cebb69c76ef17b5998f2517
arshadvkb <arshadvkb118@gmail.com>
2025-11-02 14:53:24 +0530

    updated genai model to 2.5 flash

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,7 @@
 import ai from '../config/genai.js';
 export const generateAiResponse = async (promt) => {
   const response = await ai.models.generateContent({
-    model: 'gemini-2.0-flash-001',
+    model: 'gemini-2.5-flash-001',
     contents: promt,
   });
 
