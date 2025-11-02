fe84fb8c83602c6d5f70c54869f20a9f87ae22cb
arshadvkb <arshadvkb118@gmail.com>
2025-11-02 13:56:33 +0530

    integrated genai

---
+++ src/lib/utils.js
@@ -0,0 +1,9 @@
+import ai from "../config/genai.js";
+export const generateAiResponse = async (promt) => {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash-001",
+    contents: promt,
+  });
+
+  return response.text
+};
