abf100f55d537ebd77042d56c2396988b10554c7
arshadvkb <arshadvkb118@gmail.com>
2025-11-03 10:11:28 +0530

    testing husky

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,6 +9,7 @@ const getAiRecomendation = async (req, res) => {
   const promt = `${userData}You are a personalized fitness and nutrition coach specializing in Kerala-style cuisine. Analyze the following user data and generate tailored recommendations in JSON format only. Do not include any additional text outside the JSON.`;
   const response = await generateAiResponse(promt);
   console.log(response);
+
   return res.status(200).json({ response });
 };
 export { getAiRecomendation };
