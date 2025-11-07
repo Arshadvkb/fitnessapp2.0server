0e6e5faa86238751ced62f4a16901c3f701b7bb0
arshadvkb <arshadvkb118@gmail.com>
2025-11-07 13:47:39 +0530

    added view video function

--- src/controllers/onlinevideo.controller.js
+++ src/controllers/onlinevideo.controller.js
@@ -28,9 +28,27 @@ const addvideo = async (req, res) => {
     return res.status(200).json({ message: "New video added", video });
   } catch (error) {
     console.log("error add video " + error.message);
-
     return res.status(500).json({ message: "Internal server error" });
   }
 };
 
-export { addvideo };
+const viewvideo = async (req, res) => {
+  try {
+    const video = await trainingModel.find();
+    console.log(video);
+    if (!video) {
+      return res
+        .status(400)
+        .json({ success: false, message: "No video found" });
+    }
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Video found", video });
+  } catch (error) {
+    console.log("error view video " + error.message);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export { addvideo, viewvideo };
--- src/routes/onlinevideo.route.js
+++ src/routes/onlinevideo.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addvideo } from "../controllers/onlinevideo.controller.js";
+import { addvideo, viewvideo } from "../controllers/onlinevideo.controller.js";
 import uploadlarge from "../middleware/largefile.js";
 
 const videoRouter = express.Router();
 
 videoRouter.post("/add/:id", uploadlarge.single("file"), addvideo);
+videoRouter.get("/view", viewvideo);
 
 export default videoRouter;
